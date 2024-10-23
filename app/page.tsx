8a61a3d20e62ba29777d8100253f45f7e42e6d00
Gøkmen Øzbayir <107099606+gokm8@users.noreply.github.com>
2024-10-23 14:32:43 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Hero from '@/components/home-page/hero'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
---
+++ components/home-page/hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='bg-background text-foreground'>
+      <h1 className='text-4xl font-bold'>Hero</h1>
+      <p className='mt-4'>Dette er hero-sektionen.</p>
+    </div>
+  )
+}
